cd588d80a824132e92ec748fa3bf82e3a3629663
azimenov <113203599+azimenov@users.noreply.github.com>
2023-11-11 23:32:46 +0600

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/service/EmployeeServiceImpl.java
@@ -0,0 +1,69 @@
+package com.example.backend.service;
+
+import com.example.backend.entity.EmployeeEntity;
+import com.example.backend.models.Employee;
+import com.example.backend.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    private final EmployeeRepository employeeRepository;
+    @Override
+    public Employee createEmployee(Employee employee) {
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+
+        BeanUtils.copyProperties(employee, employeeEntity);
+        employeeRepository.save(employeeEntity);
+        return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities
+                = employeeRepository.findAll();
+
+        List<Employee> employees = employeeEntities
+                .stream()
+                .map(emp -> new Employee(
+                        emp.getId(),
+                        emp.getFirstName(),
+                        emp.getLastName(),
+                        emp.getEmailId()))
+                .collect(Collectors.toList());
+        return employees;
+    }
+
+    @Override
+    public boolean deleteEmployee(Long id) {
+        EmployeeEntity employee = employeeRepository.findById(id).get();
+        employeeRepository.delete(employee);
+        return true;
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        EmployeeEntity employeeEntity
+                = employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee) {
+        EmployeeEntity employeeEntity
+                = employeeRepository.findById(id).get();
+        employeeEntity.setEmailId(employee.getEmailId());
+        employeeEntity.setFirstName(employee.getFirstName());
+        employeeEntity.setLastName(employee.getLastName());
+
+        employeeRepository.save(employeeEntity);
+        return employee;
+    }
+}
